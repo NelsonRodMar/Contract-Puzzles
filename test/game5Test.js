8236dce6a72699892ecc71fc6defe4bf3c216cff
NelsonRodMar <contact@nelsonrodmar.fr>
2022-12-23 22:20:08 +0100

    feat : found solution for the five game

--- test/game5Test.js
+++ test/game5Test.js
@@ -1,20 +1,28 @@
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { assert } = require('chai');
+const {boolean} = require("hardhat/internal/core/params/argumentTypes");
 
 describe('Game5', function () {
   async function deployContractAndSetVariables() {
     const Game = await ethers.getContractFactory('Game5');
     const game = await Game.deploy();
+    const threshold = BigInt(0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf);
+    let signer, found = false;
 
-    return { game };
+    while(found === false) {
+      signer = await ethers.Wallet.createRandom();
+      signer = await signer.connect(ethers.provider);
+      if (BigInt(`${await signer.getAddress()}`) < threshold) { found = true; }
+    }
+
+    await ethers.provider.getSigner(0).sendTransaction({to: signer.address, value: ethers.utils.parseEther("1")});
+
+    return { game, signer };
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
-
+    const { game, signer } = await loadFixture(deployContractAndSetVariables);
     // good luck
-
-    await game.win();
-
+    await game.connect(signer).win();
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
   });
